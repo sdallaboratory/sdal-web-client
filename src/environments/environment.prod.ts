54f18113fb0f2b7aabb980d69d7c1af351cd634d
Sergei Solovev <sergey.soloviev@inbox.ru>
2019-09-14 10:02:16 +0300

    env backend origin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  backendOrigin: 'http://95.216.188.201:3000',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  backendOrigin: 'http://95.216.188.201:3000',
 };
 
 /*
