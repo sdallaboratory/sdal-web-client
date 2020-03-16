3fa3da97a32e9b91ace122b5753072953e990697
Solovev Serg <sergey.soloviev@inbox.ru>
2020-03-16 17:00:58 +0300

    prod backend origin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // backendOrigin: 'https://sdal.pw',
-  backendOrigin: 'http://localhost:3000',
+  backendOrigin: 'https://sdal.pw',
   vkAppId: 7178114,
 };
