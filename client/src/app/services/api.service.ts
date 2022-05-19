ea406f5acf5deb1a63f5bbad9b084a2793405c25
Sergei Solovev <sergey.soloviev@inbox.ru>
2022-05-19 12:49:38 +0300

    fix requests to backend api in prod

--- client/src/app/services/api.service.ts
+++ client/src/app/services/api.service.ts
@@ -18,17 +18,17 @@ export class ApiService {
 
   // TODO: Uppercase on server-side
   public getSchedule(groupName: string): Observable<GroupSchedule> {
-    return this.http.get<GroupSchedule>(`/api/schedule/groups/${groupName.toUpperCase()}`).pipe(
+    return this.http.get<GroupSchedule>(`api/schedule/groups/${groupName.toUpperCase()}`).pipe(
       tap((schedule: GroupSchedule) => schedule.name = schedule.name.trim())
     );
   }
 
   public getGroups() {
-    return this.http.get<string[]>(`/api/schedule/groups`);
+    return this.http.get<string[]>(`api/schedule/groups`);
   }
 
   public searchStudents(query?: string, limit?: number, sessionId?: string) {
-    return this.http.get<Student[]>(`/api/students`, {
+    return this.http.get<Student[]>(`api/students`, {
       params: {
         query,
         limit,
@@ -45,23 +45,23 @@ export class ApiService {
       timestamp: new Date().getTime(),
     };
 
-    return this.http.post('/api/touch/comments', message);
+    return this.http.post('api/touch/comments', message);
   }
 
   public sendTargetsTelemetry(targetsInfo: TargetsTelemetry) {
-    return this.http.post('/api/touch/telemetry/targets', targetsInfo);
+    return this.http.post('api/touch/telemetry/targets', targetsInfo);
   }
 
   public getCurrentWeek() {
-    return this.http.get<WeekInfo>('/api/schedule/current-week');
+    return this.http.get<WeekInfo>('api/schedule/current-week');
   }
 
   public signOut(sessionId: string) {
-    return this.http.delete(`/api/touch/user/${sessionId}`);
+    return this.http.delete(`api/touch/user/${sessionId}`);
   }
 
   public getTouchUser(sessionId: string) {
-    return this.http.get<TouchUser | null>(`/api/touch/user/${sessionId}`);
+    return this.http.get<TouchUser | null>(`api/touch/user/${sessionId}`);
   }
 
 }
--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendOrigin: 'https://sdal.pw',
+  backendOrigin: '',
   vkAppId: 7178114,
 };
